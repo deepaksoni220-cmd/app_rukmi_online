1b494ec7400475facbc4f078fa042a92e8b4a1dc
deepaksoni220-cmd <deepak.soni220@gmail.com>
2026-04-10 10:49:52 +0530

    Add togglePassword function for password visibility

--- app.js
+++ app.js
@@ -772,6 +772,14 @@ function attachEmployeeEvents() {
     render(); // refresh
   });
 }
+function togglePassword() {
+  const passwordField = document.getElementById("password");
 
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+  } else {
+    passwordField.type = "password";
+  }
+}
 // Initial render
 render();
